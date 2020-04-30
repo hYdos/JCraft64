b5debff47f57d0da5ae0028a86c17acfb5ff7e7d
hYdos <haydenv06@gmail.com>
2020-04-30 11:08:06 +1000

    revert to older version of code

--- src/main/java/me/hydos/J64/Main.java
+++ src/main/java/me/hydos/J64/Main.java
@@ -1,26 +1,10 @@
 package me.hydos.J64;
 
-import me.hydos.J64.emu.util.EmuManager;
-import me.hydos.J64.minecraft.registry.BlockRegistry;
-import me.hydos.J64.minecraft.registry.ItemRegistry;
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.Identifier;
+import me.hydos.J64.util.EmuManager;
 
-public class Main implements ModInitializer {
+public class Main {
 
 	public static void main(String[] args) {
 		EmuManager.setup();
-	}
-
-	@Override
-	public void onInitialize() {
-		System.setProperty("java.awt.headless", "false");
-		BlockRegistry.registerAll();
-		ItemRegistry.registerAll();
-	}
-	
-	public static Identifier makeID(String name)
-	{
-		return new Identifier("jcraft64", name);
 	}
 }
